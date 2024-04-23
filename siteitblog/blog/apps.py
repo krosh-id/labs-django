b1f31d1082e838ecd00f20bea0fca9b5bc7ea68b
Vlad <87599468+krosh-id@users.noreply.github.com>
2024-04-23 13:37:21 +0300

    Delete siteitblog directory

--- siteitblog/blog/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogConfig(AppConfig):
-    verbose_name = 'Посты по тематикам'
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
